7ad7515df18c9bc52c9830488b72ff9009d81355
Danny Jativa <dany.jativa.ortiz@utelvt.edu.ec>
2022-09-16 12:43:56 -0500

    version2

--- suma.cpp
+++ suma.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
-using namespace std;
 int main ()
 {
 	float A,B,C;
+	cout<<"Ingrese dos numeros A B: ";
 	cin>>A>>B;
 	C=A+B;
+	cout<<"El resultado es: ";
 	cout<<C;
 	return 0;
 }
