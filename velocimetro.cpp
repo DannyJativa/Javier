22d8edd659a59b1de86ed50aae40197498f2940d
Danny Jativa <dany.jativa.ortiz@utelvt.edu.ec>
2022-09-16 12:57:54 -0500

    velocimetro

---
+++ velocimetro.cpp
@@ -0,0 +1,16 @@
+// creador: Danny Jativa
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float v,d,t;
+	cout<<"Ingrese la distancia(en km) d= "; cin>>d;
+	cout<<"Ingrese el tiempo(en horas)  t= "; cin>>t;
+	v=d/t;
+	cout<<"La velocidad calculada es de v= "<<d<<endl<<endl;
+	return 0;
+
+
+
+}
